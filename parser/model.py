fe670ff769bcaa2c451e7a35693f72afebb4c264
Jongseong Woo <22februa@gmail.com>
2018-08-30 16:50:25 +0900

    add eval_step() [issue #16], refactoring [issue #17]

--- parser/model.py
+++ parser/model.py
@@ -29,13 +29,28 @@ class Model(object):
         self.word_embedding = word_embedding
         self.pos_embedding = pos_embedding
         self.n_classes = len(rels_vocab_table)
-        
-        self.head_pad_id = tf.constant(heads_vocab_table[utils.GLOBAL_PAD_SYMBOL])
-        self.rel_pad_id = tf.constant(rels_vocab_table[utils.GLOBAL_PAD_SYMBOL])
+
+        self.head_pad_id = tf.constant(
+            heads_vocab_table[utils.GLOBAL_PAD_SYMBOL])
+        self.rel_pad_id = tf.constant(
+            rels_vocab_table[utils.GLOBAL_PAD_SYMBOL])
         self.global_step = tf.Variable(0, trainable=False, name='global_step')
-        
+
         self.build()
         self.initializer = tf.global_variables_initializer()
+        self.train_mode = tf.contrib.learn.ModeKeys.TRAIN
+        self.eval_mode = tf.contrib.learn.ModeKeys.EVAL
+
+        config_proto = tf.ConfigProto(
+            log_device_placement=False,
+            allow_soft_placement=True)
+        config_proto.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config_proto)
+        # use debug mode if debug is True
+        if self.hparams.debug:
+            self.sess = tf_debug.LocalCLIDebugWrapperSession(self.sess)
+        self.sess.run(self.initializer)
+        self.merge_summaries_and_create_writer(self.sess)
 
     def build(self):
         self.create_placeholders()
@@ -46,6 +61,11 @@ class Model(object):
         self.create_loss_op()
         self.create_train_op()
         self.create_uas_and_las_op()
+        # Check out_dir
+        if not tf.gfile.Exists(self.hparams.out_dir):
+            utils.print_out(
+                f"# Creating output directory {self.hparams.out_dir} ...")
+            tf.gfile.MakeDirs(self.hparams.out_dir)
 
     def create_placeholders(self):
         self.word_ids = tf.placeholder(
@@ -81,32 +101,61 @@ class Model(object):
 
     def create_mlp_layer(self):
         with tf.variable_scope('mlp'):
-            self.output = tf.reshape(self.output, [-1, 2*self.hparams.lstm_hidden_size])
+            self.output = tf.reshape(
+                self.output, [-1, 2*self.hparams.lstm_hidden_size])
             # MLP
             #   h_arc_head: [batch_size * seq_len, dim]
             self.h_arc_head, self.h_arc_dep, self.h_label_head, self.h_label_dep = mlp_for_arc_and_label(
                 self.hparams, self.output)
             # Reshape
             #   h_arc_head: [batch_size, seq_len, dim]
-            self.h_arc_head = tf.reshape(self.h_arc_head, [self.hparams.batch_size, -1, self.hparams.arc_mlp_units])
-            self.h_arc_dep = tf.reshape(self.h_arc_dep, [self.hparams.batch_size, -1, self.hparams.arc_mlp_units])
-            self.h_label_head = tf.reshape(self.h_label_head, [self.hparams.batch_size, -1, self.hparams.label_mlp_units])
-            self.h_label_dep = tf.reshape(self.h_label_dep, [self.hparams.batch_size, -1, self.hparams.label_mlp_units])
+            self.h_arc_head = tf.reshape(
+                self.h_arc_head, [self.hparams.batch_size, -1, self.hparams.arc_mlp_units])
+            self.h_arc_dep = tf.reshape(
+                self.h_arc_dep, [self.hparams.batch_size, -1, self.hparams.arc_mlp_units])
+            self.h_label_head = tf.reshape(
+                self.h_label_head, [self.hparams.batch_size, -1, self.hparams.label_mlp_units])
+            self.h_label_dep = tf.reshape(
+                self.h_label_dep, [self.hparams.batch_size, -1, self.hparams.label_mlp_units])
 
     def create_biaffine_layer(self):
         """ adding arc and label logits """
-        # logit for arc
+        # logit for arc and label
         with tf.variable_scope('arc'):
             W_arc = tf.get_variable('w_arc', [self.hparams.arc_mlp_units + 1, 1, self.hparams.arc_mlp_units],
                                     dtype=tf.float32, initializer=tf.orthogonal_initializer)
             self.arc_logits = add_biaffine_layer(
                 self.h_arc_dep, W_arc, self.h_arc_head, self.hparams.device, num_outputs=1, bias_x=True, bias_y=False)
-        # logit for label
+
         with tf.variable_scope('label'):
             W_label = tf.get_variable('w_label', [self.hparams.label_mlp_units + 1, self.n_classes,
                                                   self.hparams.label_mlp_units + 1], dtype=tf.float32, initializer=tf.orthogonal_initializer)
-            self.label_logits = add_biaffine_layer(self.h_label_dep, W_label, self.h_label_head,
-                                                   self.hparams.device, num_outputs=self.n_classes, bias_x=True, bias_y=True)
+            full_label_logits = add_biaffine_layer(self.h_label_dep, W_label, self.h_label_head,
+                                                   self.hparams.device, num_outputs=self.n_classes, bias_x=True, bias_y=True)  # [batch,seq_length,heads,label_classes]
+
+            # turn off the padding tensor to false
+            gold_heads = self.head_ids
+            mask = tf.cast(tf.not_equal(
+                gold_heads, self.head_pad_id), dtype=tf.int32)
+            # convert to zero for padding values => [batch, sent_len]
+            pred_arcs = tf.multiply(gold_heads, mask)
+
+            # Gather label logits from predicted or gold heads with gather
+            # need to compute batch_size, seq_length of pred_arcs(head_ids) for getting indices
+            pred_arcs_shape = tf.shape(pred_arcs)
+            batch_size = pred_arcs_shape[0]
+            seq_len = pred_arcs_shape[1]
+            batch_idx = tf.range(0, batch_size)  # [0, 1]
+            batch_idx = tf.expand_dims(batch_idx, 1)  # [[0], [1]]
+            batch_idx = tf.tile(batch_idx, [1, seq_len])  # [[0, 0], [1, 1]]
+            seq_idx = tf.range(0, seq_len)  # [0, 1]
+            seq_idx = tf.expand_dims(seq_idx, 0)  # [[0, 1]]
+            seq_idx = tf.tile(seq_idx, [batch_size, 1])  # [[0, 1], [0, 1]]
+            # [[batch_idx, seq_idx, head_idx], ...]
+            indices = tf.stack([batch_idx, seq_idx, pred_arcs], 2)
+            self.label_logits = tf.gather_nd(
+                full_label_logits, indices=indices)
+
     # compute for loss
     def create_loss_op(self):
         # TODO(jongseong): tf.summary.scalar('train/loss', self.loss)
@@ -142,82 +191,45 @@ class Model(object):
         self.summary_writer = tf.summary.FileWriter(
             self.hparams.out_dir, sess.graph)
 
-    def train(self, sentences_indexed, pos_indexed, rels_indexed, heads_padded):
-        print('#'*30)
-        print(f'sentences_indexed {sentences_indexed.shape}')
-        print(f'pos_indexed {pos_indexed.shape}')
-        print(f'rels_indexed {rels_indexed.shape}')
-        print(f'heads_padded {heads_padded.shape}')
-        print('#'*30)
+    def train_or_eval(self, sentences_indexed, pos_indexed, heads_indexed, rels_indexed, mode):
+        sequence_length = utils.get_sequence_length(
+            sentences_indexed, self.word_vocab_table[utils.GLOBAL_PAD_SYMBOL])
 
-        # Check out_dir
-        if not tf.gfile.Exists(self.hparams.out_dir):
-            utils.print_out(f"# Creating output directory {self.hparams.out_dir} ...")
-            tf.gfile.MakeDirs(self.hparams.out_dir)
+        feed_dict = {
+            self.word_ids: sentences_indexed,
+            self.pos_ids: pos_indexed,
+            self.head_ids: heads_indexed,
+            self.rel_ids: rels_indexed,
+            self.sequence_length: sequence_length,
+        }
 
-        val_sentences, val_pos, val_rels, val_heads, val_maxlen = utils.get_dataset_multiindex(
-            self.hparams.dev_filename)
+        if mode == tf.contrib.learn.ModeKeys.TRAIN:
+            fetches = [self.arc_logits, self.label_logits,
+                       self.uas, self.las, self.summary, self.global_step]
+            arc_logits, label_logits, uas, las, summary, global_step = self.sess.run(
+                fetches, feed_dict)
+            print(f'arc_logits={np.array(arc_logits).shape}')
+            print(f'label_logits={np.array(label_logits).shape}')
+            print(f'uas={uas}')
+            print(f'las={las}')
+            if global_step % 10 == 0:
+                self.summary_writer.add_summary(
+                    summary, global_step=global_step)
+        elif mode == tf.contrib.learn.ModeKeys.EVAL:
+            # TODO(jongseong): implement to write summary for dev-set
+            fetches = [self.uas, self.las, self.global_step]
+            uas, las, global_step = self.sess.run(fetches, feed_dict)
+            print(f'uas={uas}')
+            print(f'las={las}')
+        return uas, las
 
-        val_sentences_indexed = utils.get_indexed_sequences(
-            val_sentences, self.word_vocab_table, val_maxlen)
-        val_pos_indexed = utils.get_indexed_sequences(
-            val_pos, self.pos_vocab_table, val_maxlen)
-        val_rels_indexed = utils.get_indexed_sequences(
-            val_rels, self.rels_vocab_table, val_maxlen)
-        val_heads_padded = utils.get_indexed_sequences(
-            val_heads, self.heads_vocab_table, val_maxlen, just_pad=True)
+    def train_step(self, sentences_indexed, pos_indexed, heads_indexed, rels_indexed):
+        return self.train_or_eval(
+            sentences_indexed, pos_indexed, heads_indexed, rels_indexed, self.train_mode)
 
-        config_proto = tf.ConfigProto(
-            log_device_placement=False,
-            allow_soft_placement=True)
-        config_proto.gpu_options.allow_growth = True
-        sess = tf.Session(config=config_proto)
-        # use debug mode if debug is True
-        if self.hparams.debug:
-            sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-        sess.run(self.initializer)
-
-        self.merge_summaries_and_create_writer(sess)
-
-        for epoch in range(self.hparams.num_train_epochs):
-            model_loss = []
-            heads_acc = []
-            rels_acc = []
-            val_model_loss = []
-            val_heads_acc = []
-            val_rels_acc = []
-            test_model_loss = []
-            test_heads_acc = []
-            test_rels_acc = []
-            # reset progbar each epoch
-            progbar = Progbar(len(sentences_indexed))
-            sentences_indexed, pos_indexed, rels_indexed, heads_padded = shuffle(
-                sentences_indexed, pos_indexed, rels_indexed, heads_padded, random_state=0)
-                
-            for sentences_indexed_batch, pos_indexed_batch, rels_indexed_batch, heads_indexed_batch in utils.get_batch(
-                    sentences_indexed, pos_indexed, rels_indexed, heads_padded, batch_size=self.hparams.batch_size):
-                sequence_length = utils.get_sequence_length(
-                    sentences_indexed_batch, self.word_vocab_table[utils.GLOBAL_PAD_SYMBOL])
-                
-                feed_dict = {
-                    self.word_ids: sentences_indexed_batch,
-                    self.pos_ids: pos_indexed_batch,
-                    self.head_ids: heads_indexed_batch,
-                    self.rel_ids: rels_indexed_batch,
-                    self.sequence_length: sequence_length,
-                }
-
-                arc_logits, label_logits, uas, las, global_step, summary = sess.run(
-                    [self.arc_logits, self.label_logits, self.uas, self.las, self.global_step, self.summary], feed_dict=feed_dict)
-                print(f'np.array(arc_logits).shape={np.array(arc_logits).shape}')
-                print(f'np.array(label_logits).shape={np.array(label_logits).shape}')
-                print(f'uas={uas}')
-                print(f'las={las}')
-
-                if global_step % 10 == 0:
-                    self.summary_writer.add_summary(summary, global_step=global_step)
-                break
-            break
+    def eval_step(self, sentences_indexed, pos_indexed, heads_indexed, rels_indexed):
+        return self.train_or_eval(
+            sentences_indexed, pos_indexed, heads_indexed, rels_indexed, self.eval_mode)
 
 
 def add_stacked_lstm_layers(hparams, word_embedding, lengths):
@@ -297,9 +309,11 @@ def add_biaffine_layer(input1, W, input2, device, num_outputs=1, bias_x=False, b
     dim = input1_shape[2]
 
     if bias_x:
-        input1 = tf.concat([input1, tf.ones([batch_size, batch_len, 1])], axis=2)
+        input1 = tf.concat(
+            [input1, tf.ones([batch_size, batch_len, 1])], axis=2)
     if bias_y:
-        input2 = tf.concat([input2, tf.ones([batch_size, batch_len, 1])], axis=2)
+        input2 = tf.concat(
+            [input2, tf.ones([batch_size, batch_len, 1])], axis=2)
 
     nx = dim + bias_x  # 501
     ny = dim + bias_y  # 501
@@ -307,11 +321,11 @@ def add_biaffine_layer(input1, W, input2, device, num_outputs=1, bias_x=False, b
     W = tf.reshape(W, shape=(nx, num_outputs * tf.shape(W)[-1]))
     lin = tf.matmul(tf.reshape(input1, shape=(batch_size * batch_len, nx)), W)
     lin = tf.reshape(lin, shape=(batch_size, num_outputs * batch_len, ny))
-    blin = tf.matmul(lin, tf.transpose(input2, perm=[0,2,1]))
+    blin = tf.matmul(lin, tf.transpose(input2, perm=[0, 2, 1]))
     blin = tf.reshape(blin, (batch_size, batch_len, num_outputs, batch_len))
 
     if num_outputs == 1:
         blin = tf.squeeze(blin, axis=2)
     else:
-        blin = tf.transpose(blin, perm=[0,1,3,2])
+        blin = tf.transpose(blin, perm=[0, 1, 3, 2])
     return blin
