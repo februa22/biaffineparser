63e4d23b4da9291a06d39b6faa29020e8908715d
byungwoo <bart2001@naver.com>
2018-09-03 10:52:13 +0900

    1. bug fixed when making train_file (order of morph_tag and word switched), 2. using tab as sepeartor in trail_file for escaping comma inluded words

--- build_data.sh
+++ build_data.sh
@@ -8,12 +8,14 @@ dev_input_file=raw/sejong.ppos2.test.utf8.txt
 dev_output_file=raw/sejong.test.csv
 ############## END #####################
 
-#echo "building train_data"
-#python -m parser.build_data \
-#    --input_file=${train_input_file} \
-#    --output_file=${train_output_file}
-
 echo "building dev_data"
 python -m parser.build_data \
     --input_file=${dev_input_file} \
     --output_file=${dev_output_file}
+echo "building dev_data DONE"    
+
+echo "building train_data"
+python -m parser.build_data \
+    --input_file=${train_input_file} \
+    --output_file=${train_output_file}
+echo "building train_data DONE"
