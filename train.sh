5c1663a9f11747d82752a9dee0fb0fbb6cdbd32c
Jongseong Woo <22februa@gmail.com>
2018-09-04 11:06:25 +0900

    add evaluate_and_write_predictions() [issue #26]

--- train.sh
+++ train.sh
@@ -4,9 +4,10 @@
 ############## START ###################
 train_filename=data/train_conll17.csv # Path of train dataset.
 dev_filename=data/dev_conll17.csv # Path of dev dataset.
-out_dir=parser/parser_model # Store log/model files.
+out_dir=parser/model # Store log/model files.
 device=gpu # device to use
 debug=false # use debug mode
+num_train_epochs=100 # Num epochs to train.
 ############## END #####################
 
 echo "train"
@@ -15,4 +16,5 @@ python -m parser.parser \
     --dev_filename=${dev_filename}  \
     --out_dir=${out_dir}  \
     --device=${device} \
-    --debug=${debug}
+    --debug=${debug} \
+    --num_train_epochs=${num_train_epochs}
