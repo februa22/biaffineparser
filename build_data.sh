79c3a77afb6adf352545ebd84a344e254471f327
byungwoo <bart2001@naver.com>
2018-09-12 15:30:20 +0900

    build_data.py and build_data.sh for appending another file

--- build_data.sh
+++ build_data.sh
@@ -18,8 +18,17 @@ echo "building dev_data DONE"
 
 #exit 0
 
-echo "building train_data"
+echo "building train_data with sejong"
 python -m parser.build_data \
     --input_file=${train_input_file} \
     --output_file=${train_output_file}
+echo "building train_data DONE"
+
+echo "building train_data with changwon and appending"
+python -m parser.build_data \
+    --input_file=raw/changwon_dep-2.txt \
+    --output_file=${train_output_file} \
+    --delimiter=+ \
+    --mode=a \
+    --eoj_index=3
 echo "building train_data DONE"
--- parser/build_data.py
+++ parser/build_data.py
@@ -12,61 +12,59 @@ def add_arguments(parser):
                         help='Path of input file')
     parser.add_argument('--output_file', type=str,
                         help='Path of output file for training, validating, testing')
+    parser.add_argument('--delimiter', type=str, default='|',
+                        help='delimter to split eoj to tokens')
+    parser.add_argument('--mode', type=str, default='w',
+                        help='mode to write or append (w | a)')
+    parser.add_argument('--eoj_index', type=int, default=4,
+                        help='index of eoj column in raw csv file')
+
 
 def main(flags):
-    input_file = flags.input_file
-    output_file = flags.output_file
+    input_file_path = flags.input_file
+    output_file_path = flags.output_file
+    sent_id = 0
 
-    try:
-        print(f'reading input_file={input_file}')
-        column_names = ['eoj_id', 'head_id', 'label', 'eoj', 'eoj_kangwon']
-        sejong_df = pd.read_csv(input_file, sep='\t', skip_blank_lines=True,
-                                comment=';', names=column_names, quoting=csv.QUOTE_NONE)
-    except Exception as e:
-        print(
-            f'error occured while reading input_file: input_file={input_file}, error={e}')
-        print('quit')
-        exit()
+    #if appending, get last sentnece_id
+    if flags.mode == 'a':
+        with open(output_file_path, 'r', encoding='utf-8') as output_file:
+            for index, line in enumerate(output_file):
+                if not line.strip() or line.strip().startswith(';'):
+                    continue
+                line = line.strip()
+                sent_id = line.split('\t')[0]
+            print(f'appending mode: start sent_id={sent_id}')
+            print(f'appending mode: last_line={line}')
+            sent_id = int(sent_id)
 
-    print(f"writing START: output_file={output_file}")
-    with open(output_file, 'w', encoding='utf-8') as f:
-        # write header
-        f.write('\t'.join(['sent_id', 'eoj_id', 'eoj',
-                           'pos', 'head_id', 'label', 'char']) + "\n")
-        sent_id = 0
-        for index, row in sejong_df.iterrows():
-            # initiailization
-            eoj_id = row['eoj_id']
-            if eoj_id == 1:
-                sent_id += 1
-            eoj = row['eoj_kangwon']
-            #eoj_only = '|'.join([morph[:morph.rfind('/')] for morph in str(eoj).strip().split('|')])
-            char = '|'.join(['|'.join(morph[:morph.rfind('/')])
-                             for morph in str(eoj).strip().split('|')])
+    #reading and writing start
+    print(f'reading and writing START: input_file={input_file_path}, output={output_file_path}')
+    with open(input_file_path, 'r', encoding='utf-8') as input_file:
+        with open(output_file_path, flags.mode, encoding='utf-8') as output_file:
+            if flags.mode == 'w':
+                output_file.write('sent_id\teoj_id\teoj\tpos\thead_id\tlabel\tchar\n')
+            for index, line in enumerate(input_file):
+                line = line.strip()
+                if not line or line.startswith(';'):
+                    continue
+                row = line.split('\t')
+                eoj_id = row[0]
+                if int(eoj_id) == 1:
+                    sent_id += 1
+                eoj = row[flags.eoj_index]
+                char = '|'.join(['|'.join(morph[:morph.rfind('/')])
+                                 for morph in str(eoj).strip().split(flags.delimiter)])
+                head_id = row[1]
+                label = row[2]
+                pos = '|'.join([morph[morph.rfind('/')+1:]
+                                for morph in str(eoj).strip().split(flags.delimiter)])
+                output_file.write('\t'.join(
+                    [str(s) for s in [sent_id, eoj_id, eoj, pos, head_id, label, char]]) + "\n")
+                if index % 10000 == 0:
+                    print(f'writing line index={index}')
+        print('reading and writing END')
+        return
 
-            head_id = row['head_id']
-            label = row['label']
-            pos = '|'.join([morph[morph.rfind('/')+1:]
-                            for morph in str(eoj).strip().split('|')])
-
-            f.write('\t'.join(
-                [str(s) for s in [sent_id, eoj_id, eoj, pos, head_id, label, char]]) + "\n")
-            if index % 10000 == 0:
-                print(f'writing line index={index}')
-    print('writing output_file END')
-    return
-
-'''
-def normarlize(eoj):
-    words = str(eoj).strip().split('|') # 남/NNP|과/JS
-    output = []
-    for word in words:
-        morph = word[:word.rfind('/')]
-        tag = word[word.rfind('/')+1:]
-        if tag in ['SN', 'SL', 'SH']:
-            morph = 
-    return
-'''
 
 if __name__ == '__main__':
 
